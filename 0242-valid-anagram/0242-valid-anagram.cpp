5605bc339f4a5d76cf72be4a59c528d52e74e0d3
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-07-18 09:41:00 +0530

    Time: 5 ms (26.2%), Space: 9.7 MB (76.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // hash table using arrays way
+        // brute force approach 
         if(s.length() != t.length()){
             return false;
         }
-        vector<int> count(26,0);
-        for(int i = 0; i<s.length(); i++){
-            count[s[i]-'a']++;
-            count[t[i]-'a']--;
-        }
-        for(int val : count){
-            if(val != 0){
-                return false;
-            }
-        }
-        return true;
-        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
