acb0236a0fa69bb578a63ae419b07c6328845afe
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-07-19 10:32:09 +0530

    Time: 72 ms (8.24%), Space: 34.4 MB (10.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // brute force approach 
+        // optimal approach
 
         unordered_map<string, vector<string>> res;
         for(const auto& s : strs){
-            string sortedS = s;
-            sort(sortedS.begin(), sortedS.end());
-            res[sortedS].push_back(s);
+            vector<int> count(26,0);
+            for(char c : s){
+                count[c - 'a']++;
+            }
+            string key = to_string(count[0]);
+            for(int i = 1; i < 26; i++){
+                key += ',' + to_string(count[i]);
+            }
+            res[key].push_back(s);
         }
         vector<vector<string>> result;
-        for(auto& pair : res){
+        for(const auto& pair : res){
             result.push_back(pair.second);
         }
-        return result;
-
-        
+        return result;  
     }
 };
