29f70922024fc4c33ce74615001b35b6b9c62002
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-07-23 17:21:19 +0530

    Time: 3 ms (50.31%), Space: 20.2 MB (10.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // brute force approach 
+        // optimizing solution
         unordered_map<int, int> count;
-        for(int num : nums){
-            count[num]++;
+        vector<vector<int>>freq (nums.size()+1);
+        for(int n : nums){
+            count[n] = 1 + count[n];
         }
-        vector<pair<int, int>> arr;
-        for(const auto& p : count){
-            arr.push_back({p.second, p.first});
+        for(const auto& entry : count){
+            freq[entry.second].push_back(entry.first);
         }
-        sort(arr.rbegin(), arr.rend());
         vector<int> res;
-        for(int i = 0; i < k; ++i){
-            res.push_back(arr[i].second);
+        for(int i = freq.size() - 1; i > 0; i--){
+            for(int n : freq[i]){
+                res.push_back(n);
+                if(res.size() == k){
+                    return res;
+                }
+            }
         }
         return res;
         
